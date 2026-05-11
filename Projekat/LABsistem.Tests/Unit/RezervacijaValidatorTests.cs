1c374af1f58ce0cf2e22ba382a3f56b85ea36106
Haris Sadiković <hsadikovic2@etf.unsa.ba>
2026-05-11 18:20:31 +0200

    Add unit and integration tests for reservations service

--- Projekat/LABsistem.Tests/Unit/RezervacijaValidatorTests.cs
+++ Projekat/LABsistem.Tests/Unit/RezervacijaValidatorTests.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using LABsistem.Api.Validators;
 using LABsistem.Dal.Db;
 using LABsistem.Domain.Entities;
+using LABsistem.Domain.Enums;
 using LabSistem.Domain.Enums;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
@@ -24,11 +25,12 @@ namespace LABsistem.Tests.Unit
         {
             // Arrange
             using var context = GetInMemoryDbContext();
-            var kabinet = new Kabinet { ID = 1, Naziv = "Kabinet 1", Kapacitet = 10 };
+            var kabinet = new Kabinet { ID = 1, Naziv = "Kabinet 1", Kapacitet = 10, KorisnikID = 1, ObjekatID = 1 };
             var termin = new Termin 
             { 
                 ID = 1, 
                 KabinetID = 1, 
+                KreatorID = 1,
                 StatusTermina = StatusTermina.Slobodan,
                 Datum = DateTime.Now.AddDays(1),
                 VrijemePocetka = new TimeSpan(10, 0, 0),
@@ -52,11 +54,12 @@ namespace LABsistem.Tests.Unit
         {
             // Arrange
             using var context = GetInMemoryDbContext();
-            var kabinet = new Kabinet { ID = 1, Naziv = "Kabinet 1", Kapacitet = 20 };
+            var kabinet = new Kabinet { ID = 1, Naziv = "Kabinet 1", Kapacitet = 20, KorisnikID = 1, ObjekatID = 1 };
             var termin = new Termin 
             { 
                 ID = 1, 
                 KabinetID = 1, 
+                KreatorID = 1,
                 StatusTermina = StatusTermina.Slobodan,
                 Datum = DateTime.Now.AddDays(1),
                 VrijemePocetka = new TimeSpan(10, 0, 0),
@@ -72,6 +75,95 @@ namespace LABsistem.Tests.Unit
             await validator.ValidateRezervacija(terminId: 1, profesorId: 1, limitOsoba: 15);
 
             // Assert: No exception thrown
+        }
+
+        [Fact]
+        public async Task ValidateOtkazivanje_ThrowsException_WhenLessThan24Hours()
+        {
+            using var context = GetInMemoryDbContext();
+            var terminStart = DateTime.Now.AddHours(10);
+            var termin = new Termin
+            {
+                ID = 2,
+                ProfesorID = 2,
+                KreatorID = 1,
+                Datum = terminStart.Date,
+                VrijemePocetka = terminStart.TimeOfDay,
+                VrijemeKraja = terminStart.AddHours(1).TimeOfDay,
+                StatusTermina = StatusTermina.Rezervisan
+            };
+            context.Termini.Add(termin);
+            await context.SaveChangesAsync();
+
+            var validator = new RezervacijaValidator(context);
+
+            var ex = await Assert.ThrowsAsync<Exception>(() => validator.ValidateOtkazivanje(2, 2));
+
+            Assert.Equal("Termin se može otkazati najkasnije 24h ranije.", ex.Message);
+        }
+
+        [Fact]
+        public async Task ValidateZahtjev_ThrowsException_WhenTerminNotVisible()
+        {
+            using var context = GetInMemoryDbContext();
+            var termin = new Termin
+            {
+                ID = 3,
+                KreatorID = 1,
+                Datum = DateTime.Now.AddDays(2),
+                VrijemePocetka = new TimeSpan(9, 0, 0),
+                VrijemeKraja = new TimeSpan(10, 0, 0),
+                StatusTermina = StatusTermina.Rezervisan,
+                VidljivoStudentima = false
+            };
+            context.Termini.Add(termin);
+            await context.SaveChangesAsync();
+
+            var validator = new RezervacijaValidator(context);
+
+            var ex = await Assert.ThrowsAsync<Exception>(() => validator.ValidateZahtjev(10, 3));
+
+            Assert.Equal("Termin nije vidljiv studentima.", ex.Message);
+        }
+
+        [Fact]
+        public async Task ValidateOdgovor_ThrowsException_WhenLimitReached()
+        {
+            using var context = GetInMemoryDbContext();
+            var termin = new Termin
+            {
+                ID = 4,
+                ProfesorID = 5,
+                KreatorID = 1,
+                Datum = DateTime.Now.AddDays(2),
+                VrijemePocetka = new TimeSpan(11, 0, 0),
+                VrijemeKraja = new TimeSpan(12, 0, 0),
+                StatusTermina = StatusTermina.Rezervisan,
+                LimitOsoba = 1
+            };
+            context.Termini.Add(termin);
+            context.Zahtjevi.Add(new Zahtjev
+            {
+                TerminID = 4,
+                StudentID = 15,
+                StatusZahtjeva = StatusZahtjeva.Odobren,
+                Komentar = string.Empty
+            });
+            context.Zahtjevi.Add(new Zahtjev
+            {
+                ID = 6,
+                TerminID = 4,
+                StudentID = 16,
+                StatusZahtjeva = StatusZahtjeva.NaCekanju,
+                Komentar = string.Empty
+            });
+            await context.SaveChangesAsync();
+
+            var validator = new RezervacijaValidator(context);
+
+            var ex = await Assert.ThrowsAsync<Exception>(() => validator.ValidateOdgovor(6, 5, true));
+
+            Assert.Equal("Termin je popunjen.", ex.Message);
         }
     }
 }
